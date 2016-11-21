b895482fd403d586a3c903d62bf3f3ff950b202b
FeditskiyRoman <feditskiy@gmail.com>
2016-11-21 12:58:52 +0200

    Added animation to footer

--- dev/publicDev/home-page/homePageController.js
+++ dev/publicDev/home-page/homePageController.js
@@ -14,22 +14,21 @@
       var element = document.getElementById(elementId);
       var elementY = element.offsetTop-300;
 
-      if (window.scrollY > elementY) {
+      if (window.scrollY >= elementY) {
         element.classList.remove(oldClass);
         element.classList.add(newClass);
       }
     }
 
     window.addEventListener('scroll', function(e) {  
-      vm.animated('more-icon1', 'non', 'fadeInUp');
-      vm.animated('more-icon2', 'non', 'fadeInUp');
-      vm.animated('more-icon3', 'non', 'fadeInUp');
-      vm.animated('client-logos', 'non', 'fadeIn');
-      vm.animated('how-we-work1', 'non', 'fadeInRight');
-      vm.animated('how-we-work2', 'non', 'fadeInLeft');
-      vm.animated('how-we-work3', 'non', 'fadeInRight');
-      vm.animated('will-get', 'non', 'fadeInDown');
-
+      vm.animated('more-icon1', 'none', 'fadeInUp');
+      vm.animated('more-icon2', 'none', 'fadeInUp');
+      vm.animated('more-icon3', 'none', 'fadeInUp');
+      vm.animated('client-logos', 'none', 'fadeIn');
+      vm.animated('how-we-work1', 'none', 'fadeInRight');
+      vm.animated('how-we-work2', 'none', 'fadeInLeft');
+      vm.animated('how-we-work3', 'none', 'fadeInRight');
+      vm.animated('will-get', 'none', 'fadeInDown');
     });
 
   }
